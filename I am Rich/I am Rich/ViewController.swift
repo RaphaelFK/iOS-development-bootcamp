bc2438edde392a04eeeff83495904a61d537b9d5
Raphael <raphael_kitahara@hotmail.com>
2022-03-26 15:19:05 -0300

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Raphael Ferezin Kitahara on 08/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
